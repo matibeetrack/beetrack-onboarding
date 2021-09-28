c2e84b6639d378afd1ed6390ab0b8ee812e35294
Matías Hagen <MatiasHagen@MacBook-Air-de-Beetrack.local>
2021-09-28 11:08:07 -0300

    configuración inicial

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+})(jQuery);
+
+let boxes = document.getElementsByClassName('box').length;
+
+function save() {	
+  for(let i = 1; i <= boxes; i++){
+	  var checkbox = document.getElementById(String(i));
+    localStorage.setItem("checkbox" + String(i), checkbox.checked);	
+  }
+}
+
+//for loading
+for(let i = 1; i <= boxes; i++){
+  if(localStorage.length > 0){
+    var checked = JSON.parse(localStorage.getItem("checkbox" + String(i)));
+    document.getElementById(String(i)).checked = checked;
+  }
+}
+window.addEventListener('change', save);
+
+var scroll = new SmoothScroll('a[href*="#"]');
