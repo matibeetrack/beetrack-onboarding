9e345cbf0f24fbcccf5b70eae112c8296145a3d0
Mati <mati@Matis-Mac-mini.local>
2021-09-30 14:48:14 -0300

    success sound effect

--- designops/js/main.js
+++ designops/js/main.js
@@ -98,11 +98,32 @@ function countChecked() {
   $(".progress-bar").css("width", percentage + "%").text(percentage + "%");
 
   if(checked==count) {
-    start();
-    stop();
+    start(); //start confetti
+    playFile('../assets/sounds/success.mp3'); //play success sound
+    stop(); //stop confetti
   }
 
 }
 
 countChecked();
 $(":checkbox").click(countChecked);
+
+//Sound success
+
+const context = new window.AudioContext();
+
+function playFile(filepath) {
+
+  fetch(filepath)
+    // Read it into memory as an arrayBuffer
+    .then(response => response.arrayBuffer())
+    // Turn it from mp3/aac/whatever into raw audio data
+    .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+    .then(audioBuffer => {
+      // Now we're ready to play!
+      const soundSource = context.createBufferSource();
+      soundSource.buffer = audioBuffer;
+      soundSource.connect(context.destination);
+      soundSource.start();
+    });
+}
